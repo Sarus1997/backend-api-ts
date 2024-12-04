c69013575aaec69dc311586530d17fcb20233cee
Sarus1997 <saharat.rus@gmail.com>
2024-12-04 21:07:36 +0700

    update

--- index.ts
+++ index.ts
@@ -1,43 +1,20 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-const { SetColor, COLOR } = require('./src/config/colorUtils');
-import { get_data } from './src/model/get_data';
+import { logServerStartup, logServerRunning } from './src/config/logger';
+const { get_data } = require('./src/model/get_data');
 
 // Log server startup message
-console.log(
-  SetColor([COLOR.fg.magenta], "[Server]") +
-  " " +
-  SetColor([COLOR.fg.green], "Starting...")
-);
+logServerStartup();
 
 // Dynamically import the 'open' module
 (async () => {
-  const open = (await import('open')).default;
-
   const port: number = 8888;
   const server: Application = express();
-
-  // Serve static files from 'public' directory
   server.use(express.static(path.join(__dirname, 'public')));
-
-  // Use bodyParser for handling JSON data
   server.use(bodyParser.json());
-
-  // Define API route
   server.use('/api', get_data);
-
   server.listen(port, () => {
-    console.log(
-      SetColor([COLOR.fg.magenta], "[Server]") +
-      " " +
-      SetColor([COLOR.fg.green], "Server running...")
-    );
-    console.log(
-      SetColor([COLOR.fg.green], `Server running on http://localhost:${port}`)
-    );
-
-    // Open the browser automatically
-    open(`http://localhost:${port}`);
+    logServerRunning(port);
   });
 })();
---
+++ src/config/logger.ts
@@ -0,0 +1,22 @@
+const { SetColor, COLOR } = require('../config/colorUtils');
+
+export const logServerStartup = () => {
+  console.log(
+    `${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.green], "Server starting...")}`
+  );
+  console.log(
+    `${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.green], "Server starting...")}`
+  );
+  console.log(
+    `${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.green], "Server starting...")}`
+  );
+};
+
+export const logServerRunning = (port: number) => {
+  console.log(
+    `${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.green], "Server running...")}`
+  );
+  console.log(
+    SetColor([COLOR.fg.green], `Server running on http://localhost:${port}`)
+  );
+};
