4af860bbb2e20c8fa8204aeeb5fb17d440e50b8d
S Suwannapaond <saharat.rus@gmail.com>
2025-01-09 17:06:09 +0700

    update

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -10,32 +10,41 @@ export const logServerError = (error: any) => {
 };
 
 export const logServerRunning = (port: number) => {
+
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.red], "█████  ██████")}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "->*")}`);
   }, 0);
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.white], "██     ██  ██")}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "-->*")}`);
   }, 100);
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.blue], "█████  █████")}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "--->*")}`);
   }, 200);
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.white], "   ██  ██  ██")}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "---->* SR Server ❤️♡ ❣️♡")}`);
   }, 300);
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.red], "█████  ██   ██")}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "----->*")} ${SetColor([COLOR.fg.green], `${getTimestamp()}`)}`);
   }, 400);
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "----------------------> SR Server ❤️♡ ❣️♡")}`);
-  }, 500);
-  setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.green], "Server running...")}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "------>*")} ${SetColor([COLOR.fg.green], "Server running...")}`);
+  }, 500); setTimeout(() => {
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "------->*")} ${SetColor([COLOR.fg.green], `Running on http://localhost:${port} ⚡`)}`);
   }, 600);
+
+  function getTimestamp() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+  }
   setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.green], `Running on http://localhost:${port} ⚡`)}`);
+    console.log(`${SetColor([COLOR.fg.magenta], "[Server]")} ${SetColor([COLOR.fg.yellow], "-------->*")} ${SetColor([COLOR.fg.magenta], "***** ***** ***** ***** ***** *****")}`);
   }, 700);
-  setTimeout(() => {
-    console.log(`${SetColor([COLOR.fg.magenta], "***** ***** ***** ***** ***** ***** *****")}`);
-  }, 800);
 };
 
